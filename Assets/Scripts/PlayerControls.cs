ab7d9151c75d5da3f1f401a53b829ad7a37b13aa
Geison Biazus <geisonbiazus@gmail.com>
2014-05-29 09:36:53 -0300

    first commit

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControls : MonoBehaviour {
+
+	public float moveSpeed = 10;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector2 movement = Vector2.zero;
+
+		float h = Input.GetAxis("Horizontal");
+		float v = Input.GetAxis("Vertical");
+
+		if (Input.GetButton("Horizontal")) {
+			movement.x = h;
+		} else if (Input.GetButton("Vertical")) {
+			movement.y = v;
+		}
+
+		transform.Translate(movement * moveSpeed * Time.deltaTime);
+	
+	}
+}
