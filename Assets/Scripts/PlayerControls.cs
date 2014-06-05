e606f447aea0670cbb42e7c92c7f4c0cf4044f24
Geison Biazus <geisonbiazus@gmail.com>
2014-06-05 17:40:10 -0300

    terceiro screencast terminado

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class PlayerControls : MonoBehaviour {
 
 	public float moveSpeed = 10;
+	public Animator animator;
+	public Direction direction = Direction.Down;
 
 	// Use this for initialization
 	void Start () {
@@ -14,15 +16,45 @@ public class PlayerControls : MonoBehaviour {
 	void Update () {
 
 		Vector2 movement = Vector2.zero;
+		string animationClip = "CharacterIdleDown";
 
 		float h = Input.GetAxis("Horizontal");
 		float v = Input.GetAxis("Vertical");
 
 		if (Input.GetButton("Horizontal")) {
 			movement.x = h;
+
+			if (h < 0) {
+				animationClip = "CharacterWalkLeft";
+				direction = Direction.Left;
+			} else {
+				animationClip = "CharacterWalkRight";
+				direction = Direction.Right;
+			}
+
 		} else if (Input.GetButton("Vertical")) {
 			movement.y = v;
+
+			if (v < 0) {
+				animationClip = "CharacterWalkDown";
+				direction = Direction.Down;
+			} else {
+				animationClip = "CharacterWalkUp";
+				direction = Direction.Up;
+			}
+		} else {
+			if (direction == Direction.Left) {
+				animationClip = "CharacterIdleLeft";
+			} else if (direction == Direction.Right) {
+				animationClip = "CharacterIdleRight";
+			} else if (direction == Direction.Up) {
+				animationClip = "CharacterIdleUp";
+			} else if (direction == Direction.Down) {
+				animationClip = "CharacterIdleDown";
+			}
 		}
+
+		animator.Play(animationClip);
 
 		transform.Translate(movement * moveSpeed * Time.deltaTime);
 	
